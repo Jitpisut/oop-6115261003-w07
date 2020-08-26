2db8f96cb543877decd6c344c3a9e04dae78a3de
Jitpisut <68209254+Jitpisut@users.noreply.github.com>
2020-08-26 11:09:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace oop_6115261003_w07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Jitpisut", "Seephuak", 15000, 20, 30, 10);
+            Lecturer l1 = new Lecturer("Jitpisut", "Seephuak", 15000, 5, 10, 20, 20, 30, 20);
+            Staff s1 = new Staff("Jitpisut", "Seephuak", 15000, 5, 30, 10, 60);
+            Console.WriteLine(l1.ToString());
+            Console.WriteLine(s1.ToString());
+        }
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_6115261003_w07
+{
+    class Staff : Person
+    {
+        private double performanceLevel;
+        public double PerformanceLaval
+        {
+            get { return performanceLevel; }
+            set { performanceLevel = value; }
+        }
+        public Staff(string n, string ln, int s, int w, int i, double a, double p)
+        {
+            this.Name = n;
+            this.Lastname = ln;
+            this.Salary = s;
+            this.WorkedYear = w;
+            this.IntentYearlyActivity = i;
+            this.AssessedFromAdmin = a;
+            this.performanceLevel = p;
+        }
+
+        public double calculateSalaryRaise()
+        {
+            double score = this.AssessedFromAdmin + this.IntentYearlyActivity + this.PerformanceLaval;
+            double salaryRaise = 0;
+
+            if (score < 70)
+            {
+                return Salary;
+            }
+            else if (score <= 80)
+            {
+                salaryRaise = Salary * 0.01 + Salary;
+            }
+            else if (score <= 85)
+            {
+                salaryRaise = Salary * 0.015 + Salary;
+            }
+            else if (score <= 90)
+            {
+                salaryRaise = Salary * 0.02 + Salary;
+            }
+            else if (score <= 95)
+            {
+                salaryRaise = Salary * 0.025 + Salary;
+            }
+            else if (score > 95)
+            {
+                salaryRaise = Salary * 0.03 + Salary;
+            }
+            return salaryRaise;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + "Salary Raise : : " +
+                this.calculateSalaryRaise();
+        }
+    }
+}
